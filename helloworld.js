5d1e0106da5b04f962bf6be59f4624eb43318c5a
THIAGO SIQUEIRA QUINTINO <thiago.squintino@senacsp.edu.br>
2024-08-24 11:49:13 -0300

    second commit

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,20 @@
 console.log ("goodbye world")
+console.log (333)
+console.log (false)
+
+let numberB = 6
+let numberA = 7
+
+if (numberA > numberB){
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+console.log (numberA + numberB)
+
+console.log("a sua idade é", numberA * numberB)
+
+//var = variaveis globais
+//let = variaveis locais
